5f221cd0f6c2aa5a3beff206acd5d1aedf3c7615
Dave Kiss <1256071+davekiss@users.noreply.github.com>
2025-05-12 15:31:50 -0400

    Update prizes

--- src/components/Prizes.tsx
+++ src/components/Prizes.tsx
@@ -6,28 +6,31 @@ const Prizes = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div>
-                    <div className="bg-plum p-5 border-2 border-black flex items-center justify-center rounded-sm h-72">
+                    <div className="bg-black p-5 border-2 border-black flex items-center justify-center rounded-sm h-72">
                         <div className="relative">
-                            <img src="/images/peak-design-bag.png" alt="Backpack" width={160} height={225} className="absolute rotate-10" />
-                            <img src="/images/peak-design-bag.png" alt="Backpack" width={160} height={225} className="relative -left-2 -top-2 -rotate-4" />
+                            <img src="/images/vercel-icon-dark.png" alt="Backpack" width={160} height={225} className="" />
                         </div>
                     </div>
 
                     <h2 className="text-xl md:text-2xl mt-4 mb-6 font-vcr text-plum">Winning submission</h2>
-                    <Label>TWO MUX PEAK DESIGN BACKPACKS</Label>
-                    <p className="mb-8 text-2xl font-aeonik leading-relaxed">The best way to carry your laptop and a few snacks</p>
+                    <Label>TRIP TO VERCEL SHIP</Label>
+                    <p className="mb-8 text-2xl font-aeonik leading-relaxed">Fly to New York City and attend the Vercel conference</p>
                 </div>
 
                 <div>
                     <div className="bg-radred p-5 border-2 border-black flex items-center justify-center rounded-sm h-72">
-                        <div className="relative">
-                            <img src="/images/sonos.png" alt="Sonos speaker" width={243} height={90} />
+                        <div className="relative w-full h-72 overflow-hidden">
+                                <img src="/images/elgato-stream-deck.png" alt="Elgato" width={400} height={409} className="absolute top-2 right-0 w-26" />
+                                <img src="/images/elgato-light.png" alt="Elgato" width={1218} height={1712} className="absolute -bottom-2 left-6 w-20" />
+                                <img src="/images/elgato-light.png" alt="Elgato" width={1218} height={1712} className="absolute -bottom-4 left-16 w-20" />
+                                <img src="/images/elgato-prompter.png" alt="Elgato" width={400} height={352} className="absolute top-6 left-0 w-48" />
+                                <img src="/images/elgato-mic.png" alt="Elgato" width={200} height={400} className="absolute right-4 bottom-0 w-20" />
                         </div>
                     </div>
 
-                    <h2 className="text-xl md:text-2xl mt-4 mb-6 font-vcr text-plum">Second place</h2>
-                    <Label>Mux Sonos speaker</Label>
-                    <p className="mb-8 text-2xl font-aeonik leading-relaxed">Fill your space with sound as crisp as your code</p>
+                    <h2 className="text-xl md:text-2xl mt-4 mb-6 font-vcr text-plum">Runner up</h2>
+                    <Label>Elgato streaming bundle</Label>
+                    <p className="mb-8 text-2xl font-aeonik leading-relaxed">Prompter, 2x Key Light MK.2, Stream Deck, and Wave:3 mic.</p>
                 </div>
 
                 <div>
@@ -37,9 +40,9 @@ const Prizes = () => {
                         </div>
                     </div>
 
-                    <h2 className="text-xl md:text-2xl mt-4 mb-6 font-vcr text-plum">IRL Entrants</h2>
+                    <h2 className="text-xl md:text-2xl mt-4 mb-6 font-vcr text-plum">All participants</h2>
                     <Label>Media Chrome Thrift-tee</Label>
-                    <p className="mb-8 text-2xl font-aeonik leading-relaxed">Show up in person and walk away with some sick swag</p>
+                    <p className="mb-8 text-2xl font-aeonik leading-relaxed">Retro vibes guaranteed to help you learn to write COBOL</p>
                 </div>
 
                 <div>
